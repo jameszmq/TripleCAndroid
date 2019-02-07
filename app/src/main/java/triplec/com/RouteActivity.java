770bbc0c95d328f757921593c59f123ef6c075ad
Mingqi Zhang <jameszmq@gmail.com>
2019-02-06 18:20:05 -0800

    Added some images for buttons

--- app/src/main/java/triplec/com/RouteActivity.java
+++ app/src/main/java/triplec/com/RouteActivity.java
@@ -1,5 +1,6 @@
 package triplec.com;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -9,17 +10,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class RouteActivity extends FragmentActivity {
 
     private static final int LIST = 0;
     private static final int MAP = 1;
 
+    private FloatingActionButton addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
         SwitchFragment(MAP);
+        addButton = (FloatingActionButton) findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddToMyList();
+            }
+        });
     }
 
     public void ShowList(View v) {
@@ -42,6 +53,10 @@ public class RouteActivity extends FragmentActivity {
         }
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().replace(R.id.show, fragment).commit();
+    }
+
+    public void AddToMyList(){
+        Toast.makeText(RouteActivity.this, "Added to My Favorites", Toast.LENGTH_LONG).show();
     }
 
 }
